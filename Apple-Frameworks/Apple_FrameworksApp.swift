94db1d476661cf07591ca30c945b1d05a27700c0
izaac mac <hassandaru@gmail.com>
2021-12-11 11:58:25 +0930

    initial setup files

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Hassan Sohail Dar on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
